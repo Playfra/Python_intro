420f5bcabdd20efd81f1b6a5c631ec571fb853f7
Playfra <65603338+Playfra@users.noreply.github.com>
2020-06-14 20:37:02 +1000

    Update problem2_1.py

--- problem2_1.py
+++ problem2_1.py
@@ -4,12 +4,10 @@ Problem 1_2:
 Write a function problem1_2(x,y) that prints the sum and product of the
 numbers x and y on separate lines, the sum printing first.
 """
-#%%
 def problem1_2(x,y):
     pass # replace this pass (a do-nothing) statement with your code
 
 
-#%% 
 """
 Test run. Note that the grader program will use different numbers:
 
